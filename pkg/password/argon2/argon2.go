da14108b02c4ad7471994e53b56b11a219842ccf
Yuki Yuigishi <me@yuigishi.dev>
2024-01-13 01:10:48 +0900

    chore:fix lint

--- pkg/password/argon2/argon2.go
+++ pkg/password/argon2/argon2.go
@@ -3,27 +3,29 @@ package argon2
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"errors"
 	"fmt"
 	"strings"
 
-	"github.com/sopuro3/klend-back/pkg/password"
 	"golang.org/x/crypto/argon2"
+
+	"github.com/sopuro3/klend-back/pkg/password"
 )
 
+//nolint:revive
 type Argon2Encoder struct{}
 
-var (
+const (
 	algorithm        = "argon2id"
 	version          = argon2.Version
 	time      uint32 = 1
 	memory    uint32 = 64 * 1024 // 64MB
 	threads   uint8  = 4
 	keyLen    uint32 = 32
+	saltLen   uint32 = 32
 )
 
-func NewArgon2Encoder() password.Encoder {
-	return &Argon2Encoder{}
+func NewArgon2Encoder() *Argon2Encoder {
+	return new(Argon2Encoder)
 }
 
 func createHashPassword(rawPassword string, salt []byte) []byte {
@@ -34,14 +36,16 @@ func createEncodedPassword(hashedPassword, salt []byte) password.EncodedPassword
 	base64Salt := base64.RawStdEncoding.EncodeToString(salt)
 	base64HashedPassword := base64.RawStdEncoding.EncodeToString(hashedPassword)
 	// $<algorithm name>$v=<version>$m=<memory size>,t=<time>,p=<threads>$<b64 salt>$<b64 hash value>
-	return password.EncodedPassword(fmt.Sprintf("$%s$v=%d$m=%d,t=%d,p=%d$%s$%s", algorithm, version, memory, time, threads, base64Salt, base64HashedPassword))
+	//nolint:lll
+	hash := fmt.Sprintf("$%s$v=%d$m=%d,t=%d,p=%d$%s$%s", algorithm, version, memory, time, threads, base64Salt, base64HashedPassword)
+
+	return password.EncodedPassword(hash)
 }
 
 func (e *Argon2Encoder) EncodePassword(rawPassword string) (password.EncodedPassword, error) {
-	saltLen := 32
 	salt := make([]byte, saltLen)
-	_, err := rand.Read(salt)
-	if err != nil {
+
+	if _, err := rand.Read(salt); err != nil {
 		return password.EncodedPassword(""), fmt.Errorf("could not generate salt, %w", err)
 	}
 	// to hash
@@ -51,8 +55,6 @@ func (e *Argon2Encoder) EncodePassword(rawPassword string) (password.EncodedPass
 }
 
 func (e *Argon2Encoder) IsMatchPassword(inputPassword string, storedPassword password.EncodedPassword) (bool, error) {
-	//,errTODO implement mesplit[7
-
 	hashedStoredPassword, storedSalt, err := decodeHash(storedPassword)
 	if err != nil {
 		return false, err
@@ -63,22 +65,29 @@ func (e *Argon2Encoder) IsMatchPassword(inputPassword string, storedPassword pas
 	if string(inputHashedPassword) == string(hashedStoredPassword) {
 		return true, nil
 	}
+
 	return false, nil
 }
 
-func decodeHash(encodedPassword password.EncodedPassword) (hashedPassword, salt []byte, err error) {
+func decodeHash(encodedPassword password.EncodedPassword) ([]byte, []byte, error) {
 	split := strings.Split(string(encodedPassword), "$")
+	//nolint:gomnd
 	if len(split) != 6 {
-		return []byte(""), []byte(""), errors.New("password is invalid format")
-	}
-	decodedSalt, err := base64.RawStdEncoding.DecodeString(split[4])
-	if err != nil {
+		//nolint:goerr113
+		err := fmt.Errorf("password is invalid format")
+
 		return []byte(""), []byte(""), err
 	}
-	decodedHashedPassword, err := base64.RawStdEncoding.DecodeString(split[5])
+
+	salt, err := base64.RawStdEncoding.DecodeString(split[4])
 	if err != nil {
 		return []byte(""), []byte(""), err
 	}
 
-	return decodedHashedPassword, decodedSalt, nil
+	hashedPassword, err := base64.RawStdEncoding.DecodeString(split[5])
+	if err != nil {
+		return []byte(""), []byte(""), err
+	}
+
+	return hashedPassword, salt, nil
 }
--- pkg/password/argon2/argon2_test.go
+++ pkg/password/argon2/argon2_test.go
@@ -1,3 +1,4 @@
+//nolint:lll,wsl,loopclosure,varnamelen
 package argon2
 
 import (
@@ -17,6 +18,7 @@ import (
 // p 4
 
 func Test_createHashPassword(t *testing.T) {
+	t.Parallel()
 	type args struct {
 		rawPassword string
 		salt        []byte
@@ -30,7 +32,9 @@ func Test_createHashPassword(t *testing.T) {
 		{"success", args{"test", []byte("12345678901234567890123456789012")}, "Dh3MvyBffzwDsnqMVErNbtLMHJeuI69CvRi3OLE2Of4"},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			got := createHashPassword(tt.args.rawPassword, tt.args.salt)
 			b64Got := base64.RawStdEncoding.EncodeToString(got)
 			if !reflect.DeepEqual(b64Got, tt.want) {
@@ -41,6 +45,7 @@ func Test_createHashPassword(t *testing.T) {
 }
 
 func Test_createEncodedPassword(t *testing.T) {
+	t.Parallel()
 	hashedPassword, err := base64.RawStdEncoding.DecodeString("WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY")
 	if err != nil {
 		fmt.Println("could not decode b64 password")
@@ -57,7 +62,9 @@ func Test_createEncodedPassword(t *testing.T) {
 		{"success", args{hashedPassword, []byte("12345678901234567890123456789012")}, password.EncodedPassword("$argon2id$v=19$m=65536,t=1,p=4$MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI$WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY")},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			if got := createEncodedPassword(tt.args.hashedPassword, tt.args.salt); got != tt.want {
 				t.Errorf("createEncodedPassword() = %v, want %v", got, tt.want)
 			}
@@ -66,6 +73,7 @@ func Test_createEncodedPassword(t *testing.T) {
 }
 
 func Test_decodeHash(t *testing.T) {
+	t.Parallel()
 	wantHashedPassword, err := base64.RawStdEncoding.DecodeString("WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY")
 	if err != nil {
 		t.Error(err)
@@ -82,25 +90,39 @@ func Test_decodeHash(t *testing.T) {
 		wantErr            bool
 	}{
 		{"success", args{password.EncodedPassword("$argon2id$v=19$m=65536,t=1,p=4$MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI$WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY")}, wantHashedPassword, []byte("12345678901234567890123456789012"), false},
+		{"invalid format", args{password.EncodedPassword("$argon2id$v=19$m=65536,t=1,p=4$MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI$WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY$")}, wantHashedPassword, []byte("12345678901234567890123456789012"), true},
+		{"invalid b64 salt", args{password.EncodedPassword("$argon2id$v=19$m=65536,t=1,p=4$MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI==$WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY")}, wantHashedPassword, []byte("12345678901234567890123456789012"), true},
+		{"invalid b64 password", args{password.EncodedPassword("$argon2id$v=19$m=65536,t=1,p=4$MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI$WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY==")}, wantHashedPassword, []byte("12345678901234567890123456789012"), true},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			gotHashedPassword, gotSalt, err := decodeHash(tt.args.encodedPassword)
-			if (err != nil) != tt.wantErr {
+			if err != nil {
+				if tt.wantErr {
+					return
+				}
 				t.Errorf("decodeHash() error = %v, wantErr %v", err, tt.wantErr)
+
 				return
 			}
 			if !reflect.DeepEqual(gotHashedPassword, tt.wantHashedPassword) {
 				t.Errorf("decodeHash() gotHashedPassword = %v, want %v", gotHashedPassword, tt.wantHashedPassword)
+
+				return
 			}
 			if !reflect.DeepEqual(gotSalt, tt.wantSalt) {
 				t.Errorf("decodeHash() gotSalt = %v, want %v", gotSalt, tt.wantSalt)
+
+				return
 			}
 		})
 	}
 }
 
-func TestArgon2Encoder_IsMatchPassword(t *testing.T) {
+func TestEncoder_IsMatchPassword(t *testing.T) {
+	t.Parallel()
 	type args struct {
 		inputPassword  string
 		storedPassword password.EncodedPassword
@@ -113,14 +135,16 @@ func TestArgon2Encoder_IsMatchPassword(t *testing.T) {
 	}{
 		{"success", args{inputPassword: "password", storedPassword: password.EncodedPassword("$argon2id$v=19$m=65536,t=1,p=4$MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI$WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY")}, true, false},
 		{"fail", args{inputPassword: "failed", storedPassword: password.EncodedPassword("$argon2id$v=19$m=65536,t=1,p=4$MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI$WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY")}, false, false},
+		{"error", args{inputPassword: "password", storedPassword: password.EncodedPassword("$argon2id$v=19$m=65536,t=1,p=4$MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI$WBo5t5PvTcN/kEJbQLWhYcF4d+n+r6hSdLX+6aJymIY$")}, false, true},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			e := &Argon2Encoder{}
 			got, err := e.IsMatchPassword(tt.args.inputPassword, tt.args.storedPassword)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("IsMatchPassword() error = %v, wantErr %v", err, tt.wantErr)
-				return
 			}
 			if got != tt.want {
 				t.Errorf("IsMatchPassword() got = %v, want %v", got, tt.want)
@@ -129,45 +153,55 @@ func TestArgon2Encoder_IsMatchPassword(t *testing.T) {
 	}
 }
 
-func TestArgon2Encoder_EncodePassword(t *testing.T) {
+func TestEncoder_EncodePassword(t *testing.T) {
+	t.Parallel()
 	t.Run("check hash differences", func(t *testing.T) {
-		e := &Argon2Encoder{}
-		hash1, err := e.EncodePassword("password")
+		t.Parallel()
+		enc := &Argon2Encoder{}
+		hash1, err := enc.EncodePassword("password")
 		if err != nil {
 			t.Errorf("EncodePassword() error = %v", err)
+
 			return
 		}
 		dhash1, salt1, err := decodeHash(hash1)
 		if err != nil {
 			t.Errorf("decodeHash() error = %v", err)
+
 			return
 		}
-		hash2, err := e.EncodePassword("password")
+		hash2, err := enc.EncodePassword("password")
 		if err != nil {
 			t.Errorf("EncodePassword() error = %v", err)
+
 			return
 		}
 		dhash2, salt2, err := decodeHash(hash2)
 		if err != nil {
 			t.Errorf("decodeHash() error = %v", err)
+
 			return
 		}
 		if hash1 == hash2 {
 			t.Errorf("Generate same salt")
+
 			return
 		}
 		if string(salt1) == string(salt2) {
 			t.Errorf("Generate same salt")
+
 			return
 		}
 		if string(dhash1) == string(dhash2) {
 			t.Errorf("Generate same salt")
+
 			return
 		}
 	})
 }
 
-func TestArgon2Encoder_Argon2Encoder(t *testing.T) {
+func TestEncoder_Argon2Encoder(t *testing.T) {
+	t.Parallel()
 	type args struct {
 		inputPassword string
 	}
@@ -178,19 +212,32 @@ func TestArgon2Encoder_Argon2Encoder(t *testing.T) {
 		wantErr bool
 	}{
 		{"sequence of events", args{"password"}, true, false},
-		{"sequence of events", args{"^@}_RKyKFr4!Y^UCMa6FA,U+kjnURedZkuYhvu-s?rxAc7u6iVLR3]hJKG]yf!Hf2]Qd.D?Qyhy2s,r#!r,3dyij>h]=Hr,NuR*e"}, true, false},
-		{"sequence of events", args{"7IVQq4L0sBy2zjSgqeLy6wnDci55mYOuMr0uuv1NFuf0Zgv3Y8JP2sl6riqm8U6N30980BUqp3ISdJn9o47Vdfp24xjj9sWP6nnkaPB4Pa75Bq3QEZLmo4IFPJRVLdFB1lek1RbG8FkELNbkmfB5kwUnC1aw2w3x3ty5J165CWP7CdtasoFbpz5RpcNgQlipvhZDQxyhzVpmfkWxgJqOqWnRgOgqQFexTK2qSNHEXidxyrKlSUoGWP5IHvSbl9Y6"}, true, false},
+		{
+			"sequence of events",
+			args{"^@}_RKyKFr4!Y^UCMa6FA,U+kjnURedZkuYhvu-s?rxAc7u6iVLR3]hJKG]yf!Hf2]Qd.D?Qyhy2s,r#!r,3dyij>h]=Hr,NuR*e"},
+			true, false,
+		},
+		{
+			"sequence of events",
+			//nolint:lll
+			args{"7IVQq4L0sBy2zjSgqeLy6wnDci55mYOuMr0uuv1NFuf0Zgv3Y8JP2sl6riqm8U6N30980BUqp3ISdJn9o47Vdfp24xjj9sWP6nnkaPB4Pa75Bq3QEZLmo4IFPJRVLdFB1lek1RbG8FkELNbkmfB5kwUnC1aw2w3x3ty5J165CWP7CdtasoFbpz5RpcNgQlipvhZDQxyhzVpmfkWxgJqOqWnRgOgqQFexTK2qSNHEXidxyrKlSUoGWP5IHvSbl9Y6"},
+			true, false,
+		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			e := &Argon2Encoder{}
-			hashedPassword, err := e.EncodePassword(tt.args.inputPassword)
+			t.Parallel()
+			enc := NewArgon2Encoder()
+			hashedPassword, err := enc.EncodePassword(tt.args.inputPassword)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("EncodePassword() error = %v, wantErr %v", err, tt.wantErr)
+
 				return
 			}
-			if ok, err := e.IsMatchPassword(tt.args.inputPassword, hashedPassword); ok != tt.want || err != nil {
+			if ok, err := enc.IsMatchPassword(tt.args.inputPassword, hashedPassword); ok != tt.want || err != nil {
 				t.Errorf("IsMatchPassword() error = %v, wantErr %v", err, tt.wantErr)
+
 				return
 			}
 		})
