94fe0f655c71d601be175da79154143dd3290b9c
Yuki Yuigishi <me@yuigishi.dev>
2024-01-15 02:17:27 +0900

    chore:separate migration script

--- main.go
+++ main.go
@@ -7,39 +7,16 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
 	"github.com/sopuro3/klend-back/pkg/api"
-	"github.com/sopuro3/klend-back/pkg/model"
 	"github.com/sopuro3/klend-back/pkg/repository"
 
 	_ "github.com/joho/godotenv/autoload"
 )
-
-//nolint:wrapcheck
-func AutoMigrate(db *gorm.DB) error {
-	if err := db.AutoMigrate(&model.User{}); err != nil {
-		return err
-	}
-
-	if err := db.AutoMigrate(&model.Issue{}); err != nil {
-		return err
-	}
-
-	if err := db.AutoMigrate(&model.LoanEntry{}); err != nil {
-		return err
-	}
-
-	if err := db.AutoMigrate(&model.Equipment{}); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
@@ -144,45 +121,4 @@ func handlerInit(e *echo.Echo, db *gorm.DB) {
 	group.POST("/equipment", equipmentHandler.PostNewEquipment)
 	group.GET("/equipment/:equipmentID", equipmentHandler.GetEquipmentByID)
 	group.PUT("/equipment/:equipmentID", equipmentHandler.PutEquipmentByID)
-}
-
-func Seed(db *gorm.DB) {
-	var count int64
-
-	db.Model(&model.Equipment{}).Count(&count)
-
-	if count > 0 {
-		return
-	}
-
-	//nolint:gomnd,lll
-	equipments := []*model.Equipment{
-		{Model: model.Model{ID: uuid.MustParse("018c7b9f8c55708f803527a5528e83ed")}, Name: "角スコップ", MaxQuantity: 20, Note: "てすとてすとてすと"},
-		{Model: model.Model{ID: uuid.MustParse("018c7ba8d2df7adcaf3dbe411ce1cb60")}, Name: "バケツ", MaxQuantity: 99, Note: "てすとてすとてすと"},
-	}
-
-	//nolint:gomnd,lll
-	loanEntries := []*model.LoanEntry{
-		{Model: model.Model{ID: uuid.MustParse("018cf5eb-c686-75b7-8413-1d61612bd1b9")}, EquipmentID: uuid.MustParse("018c7b9f8c55708f803527a5528e83ed"), Quantity: 10},
-		{Model: model.Model{ID: uuid.MustParse("018cf5ec-0faa-7378-9dea-e832670afdc7")}, EquipmentID: uuid.MustParse("018c7ba8d2df7adcaf3dbe411ce1cb60"), Quantity: 20},
-		// {Model: model.Model{ID: uuid.MustParse("018cfd8b-ee64-71c2-929c-e8d1cca5c2f0")}, EquipmentID: uuid.MustParse("018c7b9f8c55708f803527a5528e83ed"), Quantity: 5},
-	}
-
-	//nolint:lll
-	issues := []*model.Issue{
-		{Model: model.Model{ID: uuid.MustParse("018c7765-ffd5-724f-aa7f-227175f54d3f")}, Address: "小森野1-1-1", Name: "久留米太郎", DisplayID: "0001", Status: "survey", Note: "テストデータ", LoanEntries: loanEntries[0:2]},
-		// {Model: model.Model{ID: uuid.MustParse("018cfd89-67cd-77f2-955e-da5439bb8d7e")}, Address: "小森野1-1-2", Name: "久留米次郎", DisplayID: "0002", Status: "start", Note: "テストデータ", IsConfirmed: true, LoanEntries: loanEntries[2:]},
-	}
-
-	if err := db.Create(&equipments).Error; err != nil {
-		slog.Warn("%+v", err)
-	}
-
-	if err := db.Create(&loanEntries).Error; err != nil {
-		slog.Warn("%+v", err)
-	}
-
-	if err := db.Create(&issues).Error; err != nil {
-		slog.Warn("%+v", err)
-	}
 }
