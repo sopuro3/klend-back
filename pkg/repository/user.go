fc3a0238ab6730cd51d646226808b06271c396e6
Yuki Yuigishi <me@yuigishi.dev>
2024-01-15 02:18:08 +0900

    feat:user Repository

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 
@@ -22,42 +23,92 @@ type userRepository struct {
 	db *gorm.DB
 }
 
-func (u *userRepository) Find(id uuid.UUID) (*model.User, error) {
-	//TODO implement me
-	panic("implement me")
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db}
 }
 
-func (u *userRepository) FindByUserID(userID string) (*model.User, error) {
-	//TODO implement me
-	panic("implement me")
+func (u *userRepository) Find(id uuid.UUID) (*model.User, error) {
+	user := model.User{Model: model.Model{ID: id}}
+	if err := u.db.Take(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (u *userRepository) FindByUserID(externalUserID string) (*model.User, error) {
+	user := model.User{}
+	if err := u.db.Where("external_user_id = ?", externalUserID).Take(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (u *userRepository) FindByUserName(userName string) (*model.User, error) {
-	//TODO implement me
-	panic("implement me")
+	user := model.User{}
+	if err := u.db.Where("user_name = ?", userName).Take(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (u *userRepository) FindByEmail(email string) (*model.User, error) {
-	//TODO implement me
-	panic("implement me")
+	user := model.User{}
+	if err := u.db.Where("email = ?", email).Take(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (u *userRepository) FindAll() ([]*model.User, error) {
-	//TODO implement me
-	panic("implement me")
+	var users []*model.User
+	result := u.db.Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return nil, nil
+	}
+
+	return users, nil
 }
 
 func (u *userRepository) Create(user *model.User) error {
-	//TODO implement me
-	panic("implement me")
+	if err := u.db.Create(&user).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (u *userRepository) Update(user *model.User) error {
-	//TODO implement me
-	panic("implement me")
+	if err := u.db.Save(&user).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (u *userRepository) Delete(user *model.User) error {
-	//TODO implement me
-	panic("implement me")
+	if user.ID == uuid.MustParse("00000000-0000-0000-0000-000000000000") {
+		return errors.New("failed delete user. ID is nil")
+	}
+	if err := u.db.Delete(&user).Error; err != nil {
+		return err
+	}
+	return nil
 }
